b8c12bd073d8bd83305f353363a854cccf6688e2
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-06-25 21:16:04 +0300

    fix: fix table name, upvotes type, authorName default and indexes

--- src/main/java/org/onlydevs/hibento/model/Question.java
+++ src/main/java/org/onlydevs/hibento/model/Question.java
@@ -1,24 +1,25 @@
 package org.onlydevs.hibento.model;
 
-import java.time.Instant;
-import java.util.UUID;
-
-import org.hibernate.annotations.CreationTimestamp;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
-@Table
+@Table(
+    name = "question",
+    indexes = {@Index(columnList = "event_session_id"), @Index(columnList = "upvotes")})
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -27,14 +28,12 @@ public class Question {
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
 
-  @Column
-  private String content;
+  @Column private String content;
 
-  @Column(nullable = true, name = "author_name")
-  private String authorName;
+  @Column(name = "author_name", length = 100, nullable = false)
+  private String authorName = "Anonymous";
 
-  @Column
-  private Integer upvotes;
+  @Column private int upvotes;
 
   @Column(name = "created_at")
   @CreationTimestamp
@@ -43,5 +42,4 @@ public class Question {
   @ManyToOne
   @JoinColumn(name = "event_session_id", nullable = false)
   private EventSession eventSession;
-
 }
