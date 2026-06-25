067715c2973f71adee82e637fe5a932b55b45903
COCO <colomberakotonjanahary@gmail.com>
2026-06-25 15:34:05 +0300

    Feat/spring secu (#1)
    * feat: write filter chain in security conf for spring sec auth
    
    * feat: implement admin checker which will decide wether the user is an admin or not
    
    * chore: untrack .env
    
    * feat: restrict admin endpoints to users listed in hibento admins
    
    * fix: correct error message
    
    * feat: add casdoor oauth2 conf and admin email whitelist
    
    * fix: enable flyway baseline for existing database
    
    * fix: secure all application endpoints by default
    
    * chore: format
    
    * feat: implement unit test for admin checker
    
    * feat: provide env example file

---
+++ src/main/java/org/onlydevs/hibento/endpoint/rest/security/SecurityConf.java
@@ -0,0 +1,50 @@
+package org.onlydevs.hibento.endpoint.rest.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConf {
+
+  private final AdminChecker adminChecker;
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(
+            auth ->
+                auth.requestMatchers("/ping", "/health/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
+        .oauth2Login(oauth2 -> oauth2.successHandler(authenticationSuccessHandler()))
+        .logout(LogoutConfigurer::permitAll)
+        .csrf(
+            csrf ->
+                csrf.ignoringRequestMatchers(
+                    "/events/**", "/sessions/**", "/speakers/**", "/rooms/**"));
+
+    return http.build();
+  }
+
+  @Bean
+  public AuthenticationSuccessHandler authenticationSuccessHandler() {
+    return (request, response, authentication) -> {
+      var oidcUser = (OidcUser) authentication.getPrincipal();
+      String email = oidcUser.getEmail();
+      if (!adminChecker.isAdmin(email)) {
+        throw new AccessDeniedException("You are not an administrator of HIBENTO");
+      }
+      response.sendRedirect("/");
+    };
+  }
+}
