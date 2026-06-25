6de0116cc70a71392ecb276609296a6eb6413c3d
colombefioren <colomberakotonjanahary@gmail.com>
2026-06-25 12:02:05 +0300

    feat: restrict admin endpoints to users listed in hibento admins

--- src/main/java/org/onlydevs/hibento/endpoint/rest/security/SecurityConf.java
+++ src/main/java/org/onlydevs/hibento/endpoint/rest/security/SecurityConf.java
@@ -3,15 +3,20 @@ package org.onlydevs.hibento.endpoint.rest.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConf {
+
+  private final AdminChecker adminChecker;
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -23,6 +28,7 @@ public class SecurityConf {
                     .authenticated()
                     .anyRequest()
                     .denyAll())
+        .oauth2Login(oauth2 -> oauth2.successHandler(authenticationSuccessHandler()))
         .logout(LogoutConfigurer::permitAll)
         .csrf(
             csrf ->
@@ -30,5 +36,17 @@ public class SecurityConf {
                     "/events/**", "/sessions/**", "/speakers/**", "/rooms/**"));
 
     return http.build();
+  }
+
+  @Bean
+  public AuthenticationSuccessHandler authenticationSuccessHandler() {
+    return (request, response, authentication) -> {
+      var oidcUser = (OidcUser) authentication.getPrincipal();
+      String email = oidcUser.getEmail();
+      if (!adminChecker.isAdmin(email)) {
+        throw new AccessDeniedException("You're not a Vola Administrator");
+      }
+      response.sendRedirect("/payments");
+    };
   }
 }
