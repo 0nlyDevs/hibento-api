7ca8219822974c1013c91c243100a797225202ca
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-06-25 21:15:47 +0300

    fix: add explicit table name to Speaker entity

--- src/main/java/org/onlydevs/hibento/model/Speaker.java
+++ src/main/java/org/onlydevs/hibento/model/Speaker.java
@@ -1,13 +1,5 @@
 package org.onlydevs.hibento.model;
 
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -16,12 +8,18 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
-@Table
+@Table(name = "speaker")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
