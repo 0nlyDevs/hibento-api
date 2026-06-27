a4b336c7bca77060e9f1f77f2fa1c0f174cff5cf
Tsiory Jonathan <167012196+TsioryJonathan@users.noreply.github.com>
2026-06-27 15:23:13 +0300

    fix: fallback value for CORS config (#14)

--- src/main/java/org/onlydevs/hibento/endpoint/rest/security/CorsConfig.java
+++ src/main/java/org/onlydevs/hibento/endpoint/rest/security/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-  @Value("${hibento.cors.allowed-origins}")
+  @Value("${hibento.cors.allowed-origins:http://localhost:5173}")
   private String allowedOrigins;
 
   @Override
